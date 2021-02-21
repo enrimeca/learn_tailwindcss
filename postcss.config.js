0a9afac960bf1c026898355f6ac1b82fcecd3ccf
Enrique Mejia <enriquemejiacano@gmail.com>
2021-02-21 08:18:03 -0500

    adding libraries

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,16 @@
+const purgecss = require('@fullhuman/postcss-purgecss')
+
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  }
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+    purgecss({
+      content: ['./**/*.html'],
+      defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+ 
+    }),
+    require('cssnano')({
+      preset: 'default',
+    }),
+  ],
 }
